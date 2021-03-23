d08a357425ad16fc32fe0189e2aeecb3c1c36c48
Freek <freek.v.esser@gmail.com>
2021-03-23 13:31:04 +0100

    Knowledge base WIP

---
+++ CeeLearnAndDo/Controllers/KnowledgebaseController.cs
@@ -0,0 +1,17 @@
+﻿using CeeLearnAndDo.Controllers.inheretance;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo.Controllers
+{
+    public class KnowledgebaseController : AuthController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
