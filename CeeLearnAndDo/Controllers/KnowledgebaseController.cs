d371c6eb34de63e930fea3ac53ebc9412b68c5f9
freekesser <freek.v.esser@gmail.com>
2021-03-29 17:02:21 +0200

    Made the basic knowledgebase

--- CeeLearnAndDo/Controllers/KnowledgebaseController.cs
+++ CeeLearnAndDo/Controllers/KnowledgebaseController.cs
@@ -1,4 +1,5 @@
 ﻿using CeeLearnAndDo.Controllers.inheretance;
+using CeeLearnAndDo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,32 @@ namespace CeeLearnAndDo.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Knowledgebases"] = db.Knowledgebases.ToList();
             return View();
+        }
+
+        public IActionResult Show(int id)
+        {
+            ViewData["Knowledgebase"] = db.Knowledgebases.Where(k => k.Id == id).FirstOrDefault();
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string Title, string Content)
+        {
+            db.Knowledgebases.Add(new Knowledgebase {
+                Title = Title,
+                Content = Content,
+                User = user,
+            });
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
